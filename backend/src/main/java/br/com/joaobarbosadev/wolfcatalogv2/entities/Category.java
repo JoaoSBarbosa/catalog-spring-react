02ab0bced477cca5139ba6e11389bd1811c21a97
João Barbosa <contato.jsbarbosa@gmail.com>
2024-07-20 20:19:59 -0300

    Implementãção inicial da entidade Categoria

---
+++ backend/src/main/java/br/com/joaobarbosadev/wolfcatalogv2/entities/Category.java
@@ -0,0 +1,24 @@
+package br.com.joaobarbosadev.wolfcatalogv2.entities;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.io.Serializable;
+
+
+@Data
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    public Category() {}
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
