a44a5a72e97a253e9704a9d607a58c635d4751eb
João Barbosa <contato.jsbarbosa@gmail.com>
2024-07-20 19:45:46 -0300

    Estruturação do backend

---
+++ backend/src/main/java/br/com/joaobarbosadev/wolfcatalogv2/Wolfcatalogv2Application.java
@@ -0,0 +1,13 @@
+package br.com.joaobarbosadev.wolfcatalogv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wolfcatalogv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wolfcatalogv2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/joaobarbosadev/wolfcatalogv2/Wolfcatalogv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaobarbosadev.wolfcatalogv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wolfcatalogv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
